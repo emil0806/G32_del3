b27cc29d113c5023bc44fdd11d5a196c2992e7e6
Lau <mikkel2011@hotmail.com>
2023-11-05 13:01:04 +0100

    Using the cup and die from CDIO2

--- src/main/java/game/Cup.java
+++ src/main/java/game/Cup.java
@@ -1,5 +1,38 @@
 package game;
 
-public class Cup {
+    public class Cup {
 
-}
+        // Creates two dice in a cup
+        private Die die1 = new Die();
+        private Die die2 = new Die();
+    
+        // Control the current roll sum
+        private int currentRollSum;
+    
+        // Cup constructor
+          public Cup() {
+            this.currentRollSum = 0;
+        }
+    
+        // Rolls each die and sets the sum
+        public void setRollSum() {
+            die1.setDieValue();
+            die2.setDieValue();
+            this.currentRollSum = die1.getDieValue() + die2.getDieValue();
+        }
+    
+        // Gets roll sum
+        public int getRollSum() {
+            return this.currentRollSum;
+        }
+    
+        // Get value of die 1
+        public int getDie1() {
+            return die1.getDieValue();
+        }
+    
+        // Get value of die 2
+        public int getDie2() {
+            return die2.getDieValue();
+        }
+    }
--- src/main/java/game/Die.java
+++ src/main/java/game/Die.java
@@ -1,5 +1,21 @@
 package game;
 
-public class Die {
+    public class Die {
+        // Face value of die
+        private int dieValue;
+
+        // Die constructor
+        public Die() {
+        }
+    
+        // Sets die value to a random value from 1-6
+        public void setDieValue() {
+            dieValue = (int) Math.floor(Math.random() * (6 - 1 + 1) + 1);
+        }
+    
+        // Gets value of die
+        public int getDieValue() {
+            return dieValue;
+        }
 
 }
