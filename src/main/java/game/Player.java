d5435af2c080d99f57597b0cfe6249f9d3d8e82a
Emil Lauritzen <emil6008@gmail.com>
2023-11-05 21:18:54 +0100

    Creating fields for player class

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -1,5 +1,12 @@
 package game;
 
 public class Player {
-
+    private String name;
+    private int age;
+    private Account account;
+    private Piece piece;
+    private boolean inJail;
+    private boolean outOfJailCard;
+    private boolean moveByTypeCard;
+    private Field[] fieldsOwned;
 }
