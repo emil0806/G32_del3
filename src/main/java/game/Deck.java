938a5f2daa695764c593a5e720948362cb4b0ec4
William Ajspur <wilajs23@gmail.com>
2023-11-06 16:17:14 +0100

    Adding constructor to deck

--- src/main/java/game/Deck.java
+++ src/main/java/game/Deck.java
@@ -4,4 +4,28 @@ public class Deck {
 
     private Card[] cards = new Card[20];
 
+    public Deck() {
+        cards[0] = new Card();
+        cards[1] = new Card();
+        cards[2] = new Card();
+        cards[3] = new Card();
+        cards[4] = new Card();
+        cards[5] = new Card();
+        cards[6] = new Card();
+        cards[7] = new Card();
+        cards[8] = new Card();
+        cards[9] = new Card();
+        cards[10] = new Card();
+        cards[11] = new Card();
+        cards[12] = new Card();
+        cards[13] = new Card();
+        cards[14] = new Card();
+        cards[15] = new Card();
+        cards[16] = new Card();
+        cards[17] = new Card();
+        cards[18] = new Card();
+        cards[19] = new Card();
+
+    }
+
 }
