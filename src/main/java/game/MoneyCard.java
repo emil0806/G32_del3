aa58e302f1200ddf8a717795c91b3068c336883a
Emil Lauritzen <emil6008@gmail.com>
2023-11-06 13:02:26 +0100

    constructor in MoneyCard

--- src/main/java/game/MoneyCard.java
+++ src/main/java/game/MoneyCard.java
@@ -1,5 +1,14 @@
 package game;
 
-public class MoneyCard {
+public class MoneyCard extends Card {
+
+    private int amount;
+    private boolean fromBank;
+
+    public MoneyCard(String description, int amount, boolean fromBank) {
+        super(description);
+        this.amount = amount;
+        this.fromBank = fromBank;
+    }
 
 }
