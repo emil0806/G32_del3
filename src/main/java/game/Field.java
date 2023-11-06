82a656c9e0679b39c9de2b0e3acbd8ab27c935ff
Frebbers <frederik.bode@hendrichsen.dk>
2023-11-06 17:06:03 +0100

    Update Field.java

--- src/main/java/game/Field.java
+++ src/main/java/game/Field.java
@@ -1,5 +1,21 @@
 package game;
 
-public class Field {
+import com.sun.tools.javac.comp.Resolve;
+
+abstract class Field {
+    public String name;
+    public int location;
+    public String description;
+
+    public Field(String name, int location, String description) {
+        this.name = name;
+        this.location = location;
+        this.description = description;
+    }
+
+    //Define getters
+    public String getName() {return name;}
+    public String getDescription() {return description;}
+    public int getLocation() {return location; }
 
 }
