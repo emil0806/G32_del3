3c7066d57a3a7c5b4e17738b2313c16013b175a0
Julius <sondergaardjulius@gmail.com>
2023-11-06 13:08:52 +0100

    Piece class created

--- src/main/java/game/Piece.java
+++ src/main/java/game/Piece.java
@@ -1,5 +1,32 @@
 package game;
 
 public class Piece {
+    private String type;      // The type of the game piece 
+    private Field location;   // The current location of the game piece on the board.
 
+    // Constructor to initialize the type and location when creating a Piece object.
+    public Piece(String type, Field location) {
+        this.type = type;
+        this.location = location;
+    }
+
+    // Getter for retrieving the type of the game piece.
+    public String getType() {
+        return type;
+    }
+
+    // Setter for updating the type of the game piece.
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    // Getter for retrieving the current location of the game piece on the board.
+    public Field getLocation() {
+        return location;
+    }
+
+    // Setter for updating the location of the game piece on the board.
+    public void setLocation(Field location) {
+        this.location = location;
+    }
 }
