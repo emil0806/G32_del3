e9b8830d3e1ac03d4f8c0a6b7c72293167afd202
Emil Lauritzen <emil6008@gmail.com>
2023-11-07 11:46:55 +0100

    fields in game

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -2,4 +2,13 @@ package game;
 
 public class Game {
 
+    private Player[] players;
+    private Board board;
+    private Cup cup;
+    private int numberOfPlayers;
+
+    public void startGame() {
+
+    }
+
 }
