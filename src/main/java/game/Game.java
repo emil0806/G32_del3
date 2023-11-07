4a2cd6d1ee12ee569703fcc6cf47b0b1f8b7a2b3
Emil Lauritzen <emil6008@gmail.com>
2023-11-07 15:22:16 +0100

    sorting playerList and implementing playGame method

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -10,6 +10,7 @@ public class Game {
     private Output output;
     private String[] pieces = new String[] { "Boat", "Car", "Dog", "Cat" };
     private int numberOfPlayers;
+    private boolean hasLoser = false;
     private Scanner scanner = new java.util.Scanner(System.in);
 
     public void startGame() {
@@ -31,6 +32,40 @@ public class Game {
             output.showPieceType(pieces[i]);
             players[i].getPiece().setLocation(board.getField(0));
         }
+
+        players = sortPlayersByAge(players);
     }
 
+    public void playGame(Player[] players) {
+        // Control game start and end
+        int turnCount = 0;
+
+        while (!(hasLoser)) {
+            playerTurn(players[turnCount], cup);
+
+            if (turnCount == players.length) {
+                turnCount = 0;
+            }
+        }
+    }
+
+    public void playerTurn(Player player, Cup cup) {
+
+    }
+
+    public Player[] sortPlayersByAge(Player[] playerList) {
+
+        Player tempPlayer;
+
+        for (int i = 0; i < playerList.length; i++) {
+            for (int j = i + 1; j < playerList.length; j++) {
+                if (players[i].getAge() > playerList[j].getAge()) {
+                    tempPlayer = playerList[i];
+                    playerList[i] = playerList[j];
+                    playerList[j] = tempPlayer;
+                }
+            }
+        }
+        return playerList;
+    }
 }
