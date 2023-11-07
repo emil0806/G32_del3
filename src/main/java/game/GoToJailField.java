53ecb92caf065bbcf68bdf86bdeb06ab3f2577f1
Frederik Bode Hendrichsen <frederik.bode@hendrichsen.dk>
2023-11-07 15:19:59 +0100

    Fixed landedonfield() and constructor

--- src/main/java/game/GoToJailField.java
+++ src/main/java/game/GoToJailField.java
@@ -1,12 +1,15 @@
 package game;
 
 public class GoToJailField extends JailField{
-    private Field jailLocation;
+    private final Field  jailLocation;
     public GoToJailField(String name, int location, String description,Field jailLocation) {
-        super(name, location, description);}
+        super(name, location, description);
+        this.jailLocation = jailLocation;}
+
 
   @override  public void landedOnField(Player player){moveToJail(player);}
     private void moveToJail(Player player){
-        player.Piece.move.setLocation(jailLocation);
+        //TODO test me
+        player.getPiece().setLocation(jailLocation);
         player.setInJail(true);}
 }
