7c9f823c944c06247975c0e0dc2d15d4c4ee2e6b
Emil Lauritzen <emil6008@gmail.com>
2023-11-09 14:42:04 +0100

    MoveToFieldCard can now move a player to chosen location

--- src/main/java/game/MoveToFieldCard.java
+++ src/main/java/game/MoveToFieldCard.java
@@ -1,5 +1,37 @@
 package game;
 
-public class MoveToFieldCard {
+import java.util.Scanner;
 
+public class MoveToFieldCard extends Card {
+
+    private int[] locations;
+
+    public MoveToFieldCard(String description, int[] locations) {
+        super(description);
+        this.locations = locations;
+    }
+
+    public void applyEffect(Player player, Player[] players, Board board) {
+        moveToField(player, board);
+    }
+
+    public void moveToField(Player player, Board board) {
+        Scanner s = new java.util.Scanner(System.in);
+        boolean correctInput = false;
+        int location = 0;
+        while (!correctInput) {
+            location = s.nextInt();
+            for (int i = 0; i < this.locations.length; i++) {
+                if (location == locations[i]) {
+                    correctInput = true;
+                    break;
+                }
+            }
+        }
+
+        int nextLocation = location + player.getPiece().getLocation().getLocation();
+        Field nextField = board.getField(location + nextLocation);
+        player.getPiece().setLocation(nextField);
+        s.close();
+    }
 }
