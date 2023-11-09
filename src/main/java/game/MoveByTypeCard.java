d3056eb4fe76301fa86efdc91a509508a072e696
Emil Lauritzen <emil6008@gmail.com>
2023-11-09 14:20:52 +0100

    storeCard and applyEffect now working together

--- src/main/java/game/MoveByTypeCard.java
+++ src/main/java/game/MoveByTypeCard.java
@@ -9,11 +9,15 @@ public class MoveByTypeCard extends Card {
         this.type = type;
     }
 
-    public void applyEffect(Player player) {
-        storeCard(player);
+    public void applyEffect(Player player, Player[] players, Board board) {
+        storeCard(players);
     }
 
-    public void storeCard(Player player) {
-        player.setMoveByTypeCard(true);
+    public void storeCard(Player[] players) {
+        for (int i = 0; i < players.length; i++) {
+            if (players[i].getPiece().getType().equals(type)) {
+                players[i].setMoveByTypeCard(true);
+            }
+        }
     }
 }
