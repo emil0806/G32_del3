20481687b8ba25180f13ea75b847a1d86f8e452a
Emil Lauritzen <emil6008@gmail.com>
2023-11-09 14:46:22 +0100

    MoveToColorCard and MoveToFieldCard moves player

--- src/main/java/game/MoveToColorCard.java
+++ src/main/java/game/MoveToColorCard.java
@@ -1,5 +1,37 @@
 package game;
 
-public class MoveToColorCard {
+import java.util.Scanner;
 
+public class MoveToColorCard extends Card {
+
+    private int[] locations;
+
+    public MoveToColorCard(String description, int[] locations) {
+        super(description);
+        this.locations = locations;
+    }
+
+    public void applyEffect(Player player, Player[] players, Board board) {
+        moveToField(player, board);
+    }
+
+    public void moveToField(Player player, Board board) {
+        Scanner s = new java.util.Scanner(System.in);
+        boolean correctInput = false;
+        int location = 0;
+        while (!correctInput) {
+            location = s.nextInt();
+            for (int i = 0; i < this.locations.length; i++) {
+                if (location == locations[i]) {
+                    correctInput = true;
+                    break;
+                }
+            }
+        }
+
+        int nextLocation = location + player.getPiece().getLocation().getLocation();
+        Field nextField = board.getField(location + nextLocation);
+        player.getPiece().setLocation(nextField);
+        s.close();
+    }
 }
--- src/main/java/game/MoveToFieldCard.java
+++ src/main/java/game/MoveToFieldCard.java
@@ -1,37 +1,19 @@
 package game;
 
-import java.util.Scanner;
-
 public class MoveToFieldCard extends Card {
 
-    private int[] locations;
+    private Field field;
 
-    public MoveToFieldCard(String description, int[] locations) {
+    public MoveToFieldCard(String description, Field field) {
         super(description);
-        this.locations = locations;
+        this.field = field;
     }
 
     public void applyEffect(Player player, Player[] players, Board board) {
-        moveToField(player, board);
+        moveToField(player);
     }
 
-    public void moveToField(Player player, Board board) {
-        Scanner s = new java.util.Scanner(System.in);
-        boolean correctInput = false;
-        int location = 0;
-        while (!correctInput) {
-            location = s.nextInt();
-            for (int i = 0; i < this.locations.length; i++) {
-                if (location == locations[i]) {
-                    correctInput = true;
-                    break;
-                }
-            }
-        }
-
-        int nextLocation = location + player.getPiece().getLocation().getLocation();
-        Field nextField = board.getField(location + nextLocation);
-        player.getPiece().setLocation(nextField);
-        s.close();
+    public void moveToField(Player player) {
+        player.getPiece().setLocation(this.field);
     }
 }
