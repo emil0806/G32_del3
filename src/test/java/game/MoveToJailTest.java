856df0cb51fb689c4d51b3c5c0f3330377561cd5
Rachid Moutiq <Moutiq@gmail.com>
2023-11-21 15:50:17 +0100

    created playertest

--- src/test/java/game/MoveToJailTest.java
+++ src/test/java/game/MoveToJailTest.java
@@ -14,17 +14,12 @@ public class PlayerTest {
     private Board b = new Board();
 
 
-
     @Test
     Field f = b.getField(18); 
-    f.landedOnField(player1, players[])
-    f.moveToJail(player1);
+    f.landedOnField(players[0], players,b);
+    f.moveToJail(player1, players[0]);
         assertEquals(player1.moveToJail(), "John");
         player1.setOutOfJailCard(true);
         playerSetinJail(false);
-
         
-        }
-    }
-
-      
+}
