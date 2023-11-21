9580e7550e11fb296f556c41243d7cdb96462874
Rachid Moutiq <Moutiq@gmail.com>
2023-11-21 15:50:01 +0100

    Test moveToJail

---
+++ src/test/java/game/MoveToJailTest.java
@@ -0,0 +1,30 @@
+package game;
+
+import static junit.testcase
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PlayerTest {
+    private Player[] players = new Player[] { new Player("John", 23, "ship", 2), new Player("John John", 21, "car", 2)};
+   
+    
+    
+    private Board b = new Board();
+
+
+
+    @Test
+    Field f = b.getField(18); 
+    f.landedOnField(player1, players[])
+    f.moveToJail(player1);
+        assertEquals(player1.moveToJail(), "John");
+        player1.setOutOfJailCard(true);
+        playerSetinJail(false);
+
+        
+        }
+    }
+
+      
